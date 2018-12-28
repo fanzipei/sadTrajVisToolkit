8ace1e719170eb1794c04e80a1bd90371a4dd998
fanzipei <fanzipei@iis.u-tokyo.ac.jp>
2018-12-28 18:51:52 +0900

    make setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='trajvistool',
+      version='0.1',
+      description='Toolkit for visualizing human trajectories',
+      url='http://github.com/sadsadcool/sadTrajVisToolkit',
+      author='Zipei Fan',
+      author_email='fanzipei@iis.u-tokyo.ac.jp',
+      license='MIT',
+      packages=['trajvistool'],
+      zip_safe=False)
